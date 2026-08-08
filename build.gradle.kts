f29d2539b57585b18551249901f5ae820ce3d15e
Goooler <wangzongler@gmail.com>
2026-08-09 00:18:43 +0800

    TODO a replacement for DumpSources

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,6 +102,8 @@ listOf(
 
 /**
  * Replacement of [Copy], which defers the source and destination configurations.
+ *
+ * TODO: https://github.com/gradle/gradle/pull/38047
  */
 @CacheableTask
 abstract class DumpSources @Inject constructor(
